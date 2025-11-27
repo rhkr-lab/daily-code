40d8d6b0d3aec7befa6bdc3423aa906898f8b1a8
rhkr-lab <zyrg11037@gmail.com>
2025-11-27 10:57:51 +0800

    day2.py
    线性表的顺序存储，静态存储
    其基本操作

--- day2.py
+++ day2.py
@@ -1,1 +1,50 @@
-print("我用 GitHub Codespaces 在云端运行的第一行代码！太牛了！")
+#include <stdio.h>
+#define MaxSize 10
+typedef struct
+{
+    Elemtype data[MaxSize];
+    int length;
+}SqList;
+void InitSize(SqList &L)
+{
+    for(int i=1;i<=MaxSize;i++)
+    {
+        L.data[i]=0;
+    }
+    L.length=0;    
+}
+int main()
+{
+    SqList L;
+    InitSize(L);
+    return 0;
+}
+// 打印链表
+void PrintList(SqList &L)
+{
+    for(int i=0;i<L.length;i++)
+    {
+        printf("%d ",L.data[i]);
+    }
+    printf("\n");
+}
+//插入元素
+bool ListInsert(SqList &L,int i,ElemType e)
+{
+    if(i<1||i>L.length+1)
+    {
+        return false;
+    }
+    if(L.length>=MaxSize)
+    {
+        return false;
+    }
+    for(int j=L.length;j>=i;j--)
+    {
+        L.data[j]=L.data[j-1];
+    }
+    L.data[i-1]=e;
+    L.length++;
+    return true;
+}
+//查找元素
