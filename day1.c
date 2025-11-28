124e00e745742d09ebef03ff1ddf79ab18ed9858
rhkr-lab <zyrg11037@gmail.com>
2025-11-28 19:36:43 +0800

    Refactor ListInsert function for correct insertion
    Fix ListInsert function to properly insert an element at the specified index and handle edge cases.

--- day1.c
+++ day1.c
@@ -5,10 +5,16 @@ typedef struct
   int length;
 }SqList;
 
-bool ListInsert(SqList &L, int i, ElemType e)
+//插入元素e到第i个位置上
+bool ListInsert(SqList &L,int i,int/ElemType e)
 {
-  if(i<1||i>L.length+1)
-    return false;
-  e=L.data[i-1];
-  
+    if(i<1||L.length+1)
+        return false;
+    if(L.length>=MaxSize)
+        return false;
+    for(int j=L.length;j>=i;j--)
+        L.data[j]=L.data[j-1];
+    L.data[i-1]=e;
+    L.length++;
+    return true;
 }
