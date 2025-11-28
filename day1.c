7184944604c1f79f46e5a47e021384b70c92cbf0
rhkr-lab <zyrg11037@gmail.com>
2025-11-28 19:53:03 +0800

    Update day1.c

--- day1.c
+++ day1.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #define MaxSize 50
 typedef struct
 {
@@ -17,4 +18,28 @@ bool ListInsert(SqList &L,int i,int/ElemType e)
     L.data[i-1]=e;
     L.length++;
     return true;
+}
+//删除第i个元素，并用e带出来该元素的值
+bool ListDelete(SqList &L,int i,int &e)
+{
+    if(i<1||i>L.length)
+        return false;
+    e=L.data[i-1];
+    for(int j=i;j<L.length;j++)
+        L.data[j-1]=L.data[j];
+    L.length--;
+    return true;
+}
+//这些基本操作的使用
+int main()
+{
+    SqList L;
+    InitList(L);
+    //插入一系列元素
+    int e=-1;
+    if(ListDelete(L,3,e))
+        printf("delete success,e=%d\n",e)
+    else
+        printf("delete failure");
+    return 0;
 }
