03c6b23eaa8146921e14b55396139a97fdf5b3b2
rhkr-lab <zyrg11037@gmail.com>
2025-11-29 11:57:56 +0800

    Update day3.c

--- day3.c
+++ day3.c
@@ -26,3 +26,19 @@ bool Empty(LinkList L)
 {
     return (L=NULL);
 }
+
+//带头结点的单链表
+typedef struct LNode
+{
+    ElemType data;
+    struct LNode * next;
+}LNode,*LinkList;
+
+bool InitList(LinkList &L)
+{
+    L=(LNode *)malloc(sizeof(LNode));
+    if(L==NULL)
+        return false;
+    L->next=NULL;
+    return true;
+}
